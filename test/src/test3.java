fde0ec67bad1112fb1526e4dcee91960d6d6c2a2
David <davidzhou@Davids-MacBook-Pro.local>
2017-01-29 00:02:42 -0500

    for loop

--- test/src/test3.java
+++ test/src/test3.java
@@ -5,7 +5,7 @@
 public class test3 {
 
     public static void main(String[] args) {
-        int [] number = {10, 20, 30, 40, 50};
+        int [] number = {10, 20, 30, 40, 50, 60};
         for(int i : number) {
             System.out.println(i);
         }

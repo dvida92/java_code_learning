82726c4a9a7b3557ef9d94208cc2c28182821e63
David <davidzhou@Davids-MacBook-Pro.local>
2017-01-29 01:06:15 -0500

    final experiment

--- test/src/Basics/Test3.java
+++ test/src/Basics/Test3.java
@@ -6,25 +6,25 @@ package Basics;
  * Other variables should use this format: e.g. myLastNameIsZhou
  */
 
-public class Test2 {
+public class Test3 {
     public int i;
     public int j;
 
     //Default Constructor: "new" will use this constructor, default value for i, j =0
-    Test2() {
+    Test3() {
 
     }
 
     //Another constructor: "new" will use this constructor
-    Test2(int i, int j) {
+    Test3(int i, int j) {
         this.i = i;
         this.j = j;
     }
 
     public static void main(String[] args) {
-        Test2 t1 = new Test2(); //t1 is a pointer that points to the memory when "new" in the heap segment
-        Test2 t2 = new Test2(); //Analysis of RAM: t1 and t2 are in code segment, whereas t1.i,t1.j,t2.i and t2.j are in heap segment.
-        Test2 t3 = new Test2(1,2);
+        Test3 t1 = new Test3(); //t1 is a pointer that points to the memory when "new" in the heap segment
+        Test3 t2 = new Test3(); //Analysis of RAM: t1 and t2 are in code segment, whereas t1.i,t1.j,t2.i and t2.j are in heap segment.
+        Test3 t3 = new Test3(1,2);
         System.out.println(t1.i);
         System.out.println(t2.j);
         System.out.println(t3.i + t3.j);
--- test/src/Basics/test.java
+++ test/src/Basics/test.java
@@ -4,7 +4,7 @@ package Basics;
  * Enhanced for loop
  */
 
-public class test {
+public class Test {
     public static void main(String[] args) {
         int [] number = {10, 20, 30, 40, 50};
         for(int i : number) {
